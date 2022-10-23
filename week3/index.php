7578761710a7a4259039444e811641816d3e897b
BERNICE LEE <leebernice38@gmail.com>
2022-10-24 00:09:57 +0800

    week3

---
+++ week3/index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+</head>
+    <body> 
+<?php echo "hello!"echo date("1");?>
+    </body>
+
+</html>
---
+++ week3/random_num.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+<style>
+    .line1{
+         color:green;
+         font-weight:italic;
+    }
+    .line2{
+        color:blue;
+        font-weight:italic;
+    }
+    .line3{
+        color:red;
+        font-weight:bold;
+    }
+    .line4{
+        color:black;
+        font-weight:bold;
+        font-style:italic;
+
+    }
+
+</style>
+</head>
+    <body> 
+<?php 
+$ran1=rand(100,200);
+$ran2=rand(100,200);
+
+echo"<span class=\"line1\">"; 
+echo $ran1 ;
+echo"</span> <br>";
+
+
+
+echo"<span class=\"line2\">";
+echo $ran2;
+echo"</span> <br>";
+
+
+
+echo"<span class=\"line3\">";
+echo ($ran1+$ran2);
+echo"</span> <br>";
+
+
+echo"<span class=\"line4\">";
+echo ($ran1*$ran2);
+echo"</span> <br>";
+ ?>
+    </body>
+
+</html>
