c01b6065e1a3a9454bdf824efddb78586d4a1404
Bernice Lee Kah Wai <114966746+neko21231@users.noreply.github.com>
2022-12-06 03:59:51 +0800

    Delete neko/neko directory

--- neko/neko/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy();
-// function that Destroys Session 
-header("Location:loginpage.php");
-?>
--- neko/neko/session.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION["user"])) {
-    header("location:loginpage.php?action=denied");
-
-}
-
-?>
